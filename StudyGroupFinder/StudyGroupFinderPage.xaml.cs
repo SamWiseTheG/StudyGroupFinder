1d48f803ad2b5f66057d6d9db6044fd796c64a86
Sam Goldstein <git@samuelgoldstein.com>
2017-12-26 16:48:44 -0800

    Initial commit

---
+++ StudyGroupFinder/StudyGroupFinderPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace StudyGroupFinder
+{
+    public partial class StudyGroupFinderPage : ContentPage
+    {
+        public StudyGroupFinderPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

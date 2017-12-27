e26a4096d2baa3264fad4abf3697f8680511a322
Sam Goldstein <git@samuelgoldstein.com>
2017-12-26 17:44:29 -0800

    Menu and 2 pages started

--- StudyGroupFinder/StudyGroupFinderPage.xaml.cs
+++ StudyGroupFinder/StudyGroupFinderPage.xaml.cs
@@ -2,11 +2,29 @@
 
 namespace StudyGroupFinder
 {
-    public partial class StudyGroupFinderPage : ContentPage
+    public partial class StudyGroupFinderPage : MasterDetailPage
     {
         public StudyGroupFinderPage()
         {
             InitializeComponent();
+            Detail = new NavigationPage(new Account());
+            IsPresented = false;
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Detail = new NavigationPage(new Account());
+            IsPresented = false;
+
+
+        }
+
+        void Handle_Clicked2(object sender, System.EventArgs e)
+        {
+            Detail = new NavigationPage(new Page2());
+            IsPresented = false;
+
+
         }
     }
 }

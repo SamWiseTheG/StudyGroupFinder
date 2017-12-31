1df0e22b2ea6017ba0e64398a3c999fd69d20075
Burianu <xibriansonnenbergix@gmail.com>
2017-12-30 17:16:27 -0700

    Changed database script.

--- StudyGroupFinder.Data/Scripts/database_script.sql
+++ StudyGroupFinder.Data/Scripts/database_script.sql
@@ -4,65 +4,55 @@ USE StudyGroup;
 
 DROP TABLE IF EXISTS Students;
 CREATE TABLE Students (
-  student_id int NOT NULL AUTO_INCREMENT,
-  student_username varchar(50) NOT NULL,
-  student_password varchar(50) NOT NULL,
-  student_email varchar(50) NOT NULL,
-  student_fname varchar(30) DEFAULT NULL,
-  student_lname varchar(30) DEFAULT NULL,
-  student_major varchar(30) DEFAULT NULL,  
-  student_phone varchar(11) DEFAULT NULL,
-  UNIQUE(student_username),
-  UNIQUE(student_email),
-  PRIMARY KEY (student_id)
+  ID int NOT NULL AUTO_INCREMENT,
+  Username varchar(50) NOT NULL,
+  Password varchar(50) NOT NULL,
+  Email varchar(50) NOT NULL,
+  Fname varchar(30) NOT NULL,
+  Lname varchar(30) NOT NULL,
+  Major varchar(30) DEFAULT NULL,
+  Phone varchar(11) DEFAULT NULL,
+  UNIQUE(Username),
+  UNIQUE(Email),
+  UNIQUE(Phone),
+  PRIMARY KEY (ID)
 
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS Courses;
 CREATE TABLE Courses (
-  course_id int NOT NULL,
-  course_name varchar(50) DEFAULT NULL,
-  PRIMARY KEY (course_id)
+  ID int NOT NULL AUTO_INCREMENT,
+  Name varchar(50) DEFAULT NULL,
+  PRIMARY KEY (ID)
 
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS Sections;
 CREATE TABLE Sections (
-  section_id int NOT NULL,
-  course_id int NOT NULL,
-  section_name varchar(50) DEFAULT NULL,
-  PRIMARY KEY (section_id),
-  FOREIGN KEY (course_id)
-      REFERENCES Courses (course_id)
+  ID int NOT NULL AUTO_INCREMENT,
+  Course_ID int NOT NULL,
+  Section_Name varchar(50) DEFAULT NULL,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (Course_ID)
+      REFERENCES Courses (ID)
 
-) ENGINE=InnoDB DEFAULT CHARSET=latin1; 
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS Groups;
 CREATE TABLE Groups (
-  group_id int NOT NULL,
-  PRIMARY KEY (group_id)
+  ID int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (ID)
 
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS StudentGroups;
-CREATE TABLE StudentGroups ( 
-  student_id int NOT NULL, 
-  group_id int NOT NULL,
-  PRIMARY KEY (student_id, group_id),
-  FOREIGN KEY (group_id) 
-      REFERENCES Groups(group_id),
-  FOREIGN KEY (student_id) 
-      REFERENCES Students(student_id)
+CREATE TABLE StudentGroups (
+  Student_ID int NOT NULL,
+  Group_ID int NOT NULL,
+  PRIMARY KEY (Student_ID, Group_ID),
+  FOREIGN KEY (Group_ID)
+      REFERENCES Groups(ID),
+  FOREIGN KEY (Student_ID)
+      REFERENCES Students(ID)
 
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-DROP TABLE IF EXISTS StudentSections;
-CREATE TABLE StudentClasses ( 
-  student_id int NOT NULL, 
-  section_id int NOT NULL,
-  PRIMARY KEY (student_id, section_id),
-  FOREIGN KEY (section_id) 
-      REFERENCES Sections(section_id),
-  FOREIGN KEY (student_id) 
-      REFERENCES Students(student_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
